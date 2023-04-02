a39575579f03769bc4454249520b76c3da30d89d
mahanshAdtiya <mahansh21334@iiitd.ac.in>
2023-04-02 17:07:48 +0530

    Removed Redundant files

--- portfolio/src/components/LeftCol.jsx
+++ portfolio/src/components/LeftCol.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function LeftCol({className}) {
     return (
-        <ul className={`col-container flex justify-center flex-col items-center ${className}`}>
+        <ul className={`flex justify-center flex-col items-center ${className}`}>
             <li className=''>
                 <a target="_blank" rel="noreferrer" href="https://github.com/mahanshAdtiya"><i className="fab fa-github-alt text-2xl transform hover:-translate-y-1 transition ease-in hover:text-red-500" /></a>
             </li>
@@ -10,7 +10,7 @@ function LeftCol({className}) {
             <li className='py-4'>
                 <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/mahansh-aditya-015915238/"><i className="fab fa-linkedin-in text-2xl transform hover:-translate-y-1 transition ease-in hover:text-red-500" /></a>
             </li>
-            <li className=" col-underline w-1 h-40 rounded-xl bg-red-200"></li>
+            <li className="w-1 h-40 rounded-xl bg-red-200"></li>
         </ul>
     )
 }
--- portfolio/src/components/RightCol.jsx
+++ portfolio/src/components/RightCol.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function RightCol({className}) {
     return (
-        <ul className={`col-container right ${className}`}>
+        <ul className={`${className}`}>
             <li className="col-content rotate-90 transform hover:-translate-y-1 transition ease-in">
-                <a target="_blank" rel="noreferrer" href="mailto:mahansh21334@iiitd.ac.in" className='text-lg  hover:text-red-500'>mahansh21334@iiitd.ac.in</a>
+                <a target="_blank" rel="noreferrer" href="mailto:mahansh21334@iiitd.ac.in" className='hover:text-red-500'>mahansh21334@iiitd.ac.in</a>
             </li>
-            <li className="col-underline "></li>
+            <li className=""></li>
         </ul>
     )
 }
